1669d978a75aa2561cda4db1a9952e1c74a9a5ce
Sameer Asolkar <54775671+sameerasolkar@users.noreply.github.com>
2019-11-10 19:11:44 +0530

    added sameer4

--- spring-boot-app/src/main/resources/myData.sql
+++ spring-boot-app/src/main/resources/myData.sql
@@ -5,3 +5,4 @@ insert into employee(name) values ('AWS-DevOps');
 insert into employee(name) values ('Sameer');
 insert into employee(name) values ('Sameer2');
 insert into employee(name) values ('Sameer3');
+insert into employee(name) values ('Sameer4');
